048ff303683d1dfa8788c18e6278bf6be8826cb7
danielromeo <danielromeo99@gmail.com>
2025-08-14 06:34:24 +0200

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ const Home = () => {
   const services = [
     {
       title: "Custom Business Formation & Structure",
-      description: "Complete company registration(PTY LTD), Beneficial ownership(CIPC), Memorandum of Incorporation, and business structuring services tailored to your needs.",
+      description: "Complete company registration(PTY LTD), Beneficial ownership(CIPC), Memorandum of Incorporation, and business structuring services tailored to your needs.git ",
       icon: (
         <svg fill="currentColor" viewBox="0 0 20 20">
           <path d="M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4zM18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z" />
